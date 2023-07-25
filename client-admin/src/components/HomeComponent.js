b43f9b907c70e36023f0ce09e0a6eadef3c1b8c7
Hoang Thai <hoangthai22072023@gmail.com>
2023-07-25 20:23:15 +0700

    hehe

---
+++ client-admin/src/components/HomeComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="align-center">
+        <h2 className="text-center">ADMIN HOME</h2>
+        <img
+          src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif"
+          width="800px"
+          height="600px"
+          alt=""
+        />
+      </div>
+    );
+  }
+}
+export default Home;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'hoangthai22072023.vbzpfg2.mongodb.net',
+    DB_USER: 'hoangthai22072023',
+    DB_PASS: 'dXonxq6HySsFJAS5',
+    DB_DATABASE: 'hoangthai22072023',
+    JWT_SECRET: 'hoangthai',
+    JWT_EXPIRES: '86400000 ', // in milliseconds
+    EMAIL_USER: 'hoangthai22072023@gmail.com', // gmail service
+    EMAIL_PASS: 'Codingday22072023'
+  };
+  module.exports = MyConstants;
