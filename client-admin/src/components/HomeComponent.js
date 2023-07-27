d40d053808c41869bbbb17817e425be77b544282
hoangthai22072023 <140519425+hoangthai22072023@users.noreply.github.com>
2023-07-27 12:50:00 +0700

    Update HomeComponent.js

--- client-admin/src/components/HomeComponent.js
+++ client-admin/src/components/HomeComponent.js
@@ -6,7 +6,7 @@ class Home extends Component {
       <div className="align-center">
         <h2 className="text-center">ADMIN HOME</h2>
         <img
-          src="https://giphy.com/gifs/pokemon-sweet-pikachu-39GAXpLVKvYRO"
+          src="https://giphy.com/gifs/kawaii-pikachu-pika-vpawfuA6LnYeQ"
           width="800px"
           height="600px"
           alt=""
